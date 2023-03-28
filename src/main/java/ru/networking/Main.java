ade5f462a84b25fc43feff4d0386151129c33f61
Anton <hector123@bk.ru>
2023-03-28 08:32:58 +0300

    28.03.2023

--- src/main/java/ru/networking/Main.java
+++ src/main/java/ru/networking/Main.java
@@ -46,7 +46,7 @@ public class Main {
 //            client2.setCommands(commandsRaisecom);
 //            client2.start();
 
-            ClientTelnet client3 = new ClientTelnet("10.16.63.213", 23, promptRaisecom); //DCN
+            ClientTelnet client3 = new ClientTelnet("10.16.62.217", 23, promptRaisecom);
             client3.setCommands(new String[]{
                     "mgrconf\n",
                     "12345\n",
@@ -73,7 +73,7 @@ public class Main {
             String getMacAddress = String.format("show mac-address-table address 44-6a-2e-fa-dc-67%n");
             ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
-            FileWriter fileWriter = new FileWriter("10.16.63.213_mac_find.log");
+            FileWriter fileWriter = new FileWriter("10.16.62.217_mac_find.log");
 
             scheduledExecutorService.scheduleWithFixedDelay(() -> {
                 try {
